b0393b8d6e01bff4021f675c443a1823130e428e
212bypark <bwpark212@gmail.com>
2021-12-09 22:36:53 +0900

    1209-add readme

--- week01/b2309/main.go
+++ week01/b2309/main.go
@@ -1,3 +1,7 @@
+// 표준입력으로 9 개의 숫자를 받아
+// 그 중 전체 합이 100인 7개의 숫자를 뽑아 오름차순으로 출력
+// 조건 : 중복되지 않고 100이 넘지 않는 9개의 자연수가 주어지며
+// 가능한 답이 여러가지인 경우에는 아무거나 출력 (답이 없는 경우는 없다)
 package main
 
 import (
@@ -6,11 +10,13 @@ import (
 	"strconv"
 )
 
+// 표준입력으로 받는 값을 dwarfSlice에 저장하고 출력할 값을 answer에 저장
 type DwarfData struct {
 	dwarfSlice []int
 	answer     []int
 }
 
+// 표준입력 처리를 위한 함수이다. 개행을 기준으로 파싱
 func inputAction() *DwarfData {
 	d := &DwarfData{}
 
@@ -23,6 +29,7 @@ func inputAction() *DwarfData {
 	return d
 }
 
+// 조건에 만족하는 숫자를 찾아 DwarfData.answer에 저장하고 정렬
 func chooseDwarf(d *DwarfData) {
 	totalSum := 0
 	for _, val := range d.dwarfSlice {
