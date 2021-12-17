e5cac5504b7814affa50d08ba1683a1d7f45f2fb
212bypark <bwpark212@gmail.com>
2021-12-17 21:28:43 +0900

    1217

---
+++ week02/b11047/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+type myData struct {
+	R *bufio.Reader
+
+	count  int
+	total  int
+	values []int
+}
+
+func inputAction() *myData {
+	d := &myData{}
+	d.R = bufio.NewReader(os.Stdin)
+
+	fmt.Fscanln(d.R, &d.count, &d.total)
+	for i := 0; i < d.count; i++ {
+		var temp int
+		fmt.Fscan(d.R, &temp)
+		d.values = append(d.values, temp)
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(d.values)))
+	return d
+}
+
+func getMinCount() {
+	d := inputAction()
+	minCount := 0
+	for _, cur := range d.values {
+	CHECK:
+		if d.total-cur >= 0 {
+			d.total -= cur
+			minCount++
+			if d.total == 0 {
+				break
+			}
+			goto CHECK
+		}
+	}
+	fmt.Println(minCount)
+}
+
+func main() {
+	getMinCount()
+}
--- week02/b17509/main.go
+++ week02/b17509/main.go
@@ -1,8 +1,17 @@
 package main
 
+import (
+	"bufio"
+	"os"
+)
+
 type myData struct {
+	R *bufio.Reader
+	W *bufio.Writer
 }
 
 func inputAction() *myData {
+	d := &myData{}
 
+	d.R = bufio.NewReader(os.Stdin)
 }
