02e5b1bb2c0791b0752966cc4dfd4407036d2208
212bypark <bwpark212@gmail.com>
2021-12-25 23:59:52 +0900

    1225

---
+++ week03/b1992/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var scanner *bufio.Scanner
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+}
+
+func scanInt() int {
+	scanner.Scan()
+	x, _ := strconv.Atoi(scanner.Text())
+	return x
+}
+
+func main() {
+	a, b := scanInt(), scanInt()
+	fmt.Println(a + b)
+}
