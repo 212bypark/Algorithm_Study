e206c58e807b6d07cc006b043e7284c81fa350c3
212bypark <bwpark212@gmail.com>
2021-12-28 23:55:50 +0900

    1228

--- week03/b1725/main.go
+++ week03/b1725/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -17,4 +19,45 @@ func scanInt() int {
 	scanner.Scan()
 	x, _ := strconv.Atoi(scanner.Text())
 	return x
+}
+
+func getMin(arr []int) int {
+	tmp := make([]int, len(arr))
+	copy(tmp, arr)
+	sort.Ints(tmp)
+	// fmt.Println("checking:", arr)
+	// fmt.Println("checking:", tmp)
+	return tmp[0]
+}
+
+func getMaxVal(baselen int, arr []int) int {
+	val := 0
+	for i := 0; i+baselen <= len(arr); i++ {
+		tmp := getMin(arr[i:i+baselen]) * baselen
+		// fmt.Println("---> baselen=", baselen, ", i=", i, ", tmp=", tmp)
+		if tmp > val {
+			val = tmp
+		}
+	}
+	return val
+}
+
+func getMaxSize() {
+	n, val := scanInt(), 0
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		arr[i] = scanInt()
+	}
+	for baselen := 1; baselen <= n; baselen++ {
+		tmp := getMaxVal(baselen, arr)
+		if tmp > val {
+			val = tmp
+		}
+		// fmt.Println("(baselen, val)=", baselen, ", ", val)
+	}
+	fmt.Println(val)
+}
+
+func main() {
+	getMaxSize()
 }
